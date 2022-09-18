239b40c6068a20c3f4a2f3096660cbb82bc2f31f
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-09-18 16:34:17 +0900

    class

---
+++ src/classes/A.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class A {
+    private int f1;
+    protected int f2;
+    public int f3;
+
+    static final int S1 = 0;
+
+    public A() {
+        f1 = f2 = f3 = 0;
+    }
+
+    public A(int f1, int f2, int f3) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f3 = f3;
+    }
+
+    public void setF1(int f) {
+        f1 = f;
+    }
+
+    public int getF1() {
+        return f1;
+    }
+}
